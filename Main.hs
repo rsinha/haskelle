8f380a0f67b241ff4f99a6f98aa82854eab04050
Rohit Sinha <sinharo@gmail.com>
2012-07-24 17:55:29 -0700

    Pretty Printer in its own module

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 import Haskelle
+import Printer
 
 main = do
 	putStrLn (show (Haskelle.ProofTree (Haskelle.Sequent [] [Haskelle.Implication (Haskelle.Atomic "p") (Haskelle.Atomic "p")]) Haskelle.RightImpl [Haskelle.ProofTree (Haskelle.Sequent [Haskelle.Atomic "p"] [Haskelle.Atomic "p"]) Haskelle.Axiom []]))
